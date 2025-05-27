d7093392dcc92f278cf24720944cb8e7a998ae8a
lehlohonolohappymasipa <masipalh@icloud.com>
2025-05-27 23:32:24 +0200

    Add project files.

---
+++ MVCLearn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCLearn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
